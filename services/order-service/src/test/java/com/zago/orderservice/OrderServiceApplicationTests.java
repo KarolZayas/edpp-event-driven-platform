4b40248da59721993dab27f751c9fb3cfc8b0244
Karol Zayas <karol.zayas@gmail.com>
2026-04-02 17:27:20 -0600

    add order service skeleton

---
+++ services/order-service/src/test/java/com/zago/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zago.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
