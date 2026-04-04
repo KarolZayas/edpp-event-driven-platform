c83d0907aac707c6c26757e1d5ad3387e6bda2bf
Karol Zayas <karol.zayas@gmail.com>
2026-04-03 20:35:20 -0600

    milestone 2
    ✔ Retry con topics
    ✔ DLQ
    ✔ Idempotencia
    ✔ Avro downstream
    ✔ Fix real de parsing

--- services/payment-service/src/main/java/com/zago/paymentservice/consumers/PaymentConsumer.java
+++ services/payment-service/src/main/java/com/zago/paymentservice/consumers/PaymentConsumer.java
@@ -1,6 +1,9 @@
 package com.zago.paymentservice.consumers;
 
-import com.zago.edpp.events.OrderCreated;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zago.edpp.events.PaymentCompleted;
+import com.zago.edpp.events.PaymentStatus;
 import com.zago.paymentservice.entity.ProcessedEvent;
 import com.zago.paymentservice.repository.ProcessedEventRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +17,17 @@ import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Map;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class PaymentConsumer {
 
+    private final ObjectMapper objectMapper;
     private final ProcessedEventRepository processedEventRepository;
-    private final KafkaTemplate<String, OrderCreated> kafkaTemplate;
+    private final KafkaTemplate<String, PaymentCompleted> kafkaTemplateAvro;
+    private final KafkaTemplate<String, String> kafkaTemplateJson;
 
     @KafkaListener(topics = {
             "orders.created.v1",
@@ -29,35 +35,44 @@ public class PaymentConsumer {
             "payments.retry.30s.v1",
             "payments.retry.5m.v1"
     })
-    public void consume(ConsumerRecord<String, OrderCreated> record, Acknowledgment ack) {
+    public void consume(ConsumerRecord<String, String> record, Acknowledgment ack) {
 
         try {
-            OrderCreated event = record.value();
-            UUID eventId = UUID.fromString(event.getEventId());
+            JsonNode envelope = objectMapper.readTree(record.value());
+            String payloadString = envelope.get("payload").asText();
+            JsonNode event = objectMapper.readTree(payloadString);
+            UUID eventId = UUID.fromString(event.get("eventId").asText());
 
             // 🧠 Idempotencia
             if (processedEventRepository.existsById(eventId)) {
                 ack.acknowledge();
                 return;
             }
-            System.out.println("Processing order (payment): " + event.getOrderId());
+            System.out.println("Processing order (payment): " + event.get("orderId").asText());
 
             // 💥 Simular fallo
-            if (Math.random() < 0.7) {
-                throw new RuntimeException("Random failure");
-            }
+
+            // 💳 lógica de negocio
+            //processPayment(event);
+
+            // 🧬 transformar a Avro
+            var avroEvent = mapToAvro(event);
+
+            // 🚀 publicar en Kafka (Avro)
+            kafkaTemplateAvro.send("payments.completed.v1", avroEvent);
 
             // ✅ éxito
             processedEventRepository.save(new ProcessedEvent(eventId, Instant.now()));
             ack.acknowledge();
 
         } catch (Exception e) {
+            System.out.println("Error processing order (payment): " + e.getMessage());
             handleRetry(record);
             ack.acknowledge(); // IMPORTANTE: no bloquear
         }
     }
 
-    private void handleRetry(ConsumerRecord<String, OrderCreated> record) {
+    private void handleRetry(ConsumerRecord<String, String> record) {
 
         int retryCount = getRetryCount(record.headers());
 
@@ -68,13 +83,13 @@ public class PaymentConsumer {
             default -> "payments.dlq.v1";
         };
 
-        ProducerRecord<String, OrderCreated> newRecord =
+        ProducerRecord<String, String> newRecord =
                 new ProducerRecord<>(nextTopic, record.key(), record.value());
 
         newRecord.headers().add("retry-count",
                 String.valueOf(retryCount + 1).getBytes());
 
-        kafkaTemplate.send(newRecord);
+        kafkaTemplateJson.send(newRecord);
 
         System.out.println("Sent to: " + nextTopic + " retry=" + (retryCount + 1));
     }
@@ -82,5 +97,30 @@ public class PaymentConsumer {
     private int getRetryCount(Headers headers) {
         Header header = headers.lastHeader("retry-count");
         return header == null ? 0 : Integer.parseInt(new String(header.value()));
+    }
+
+    private PaymentCompleted mapToAvro(JsonNode event) {
+
+        return PaymentCompleted.newBuilder()
+                .setEventId(UUID.randomUUID().toString())
+                .setEventType("PaymentCompleted")
+                .setEventVersion("v1")
+                .setOccurredAt(System.currentTimeMillis())
+
+                .setOrderId(event.get("orderId").asText())
+                .setUserId(event.get("userId").asText())
+                .setAmount(event.get("amount").asDouble())
+
+                .setCurrency("MXN")
+                .setStatus(PaymentStatus.SUCCESS)
+
+                .setPaymentMethod("CARD")
+
+                .setMetadata(Map.of(
+                        "source", "payment-service",
+                        "env", "local"
+                ))
+
+                .build();
     }
 }
